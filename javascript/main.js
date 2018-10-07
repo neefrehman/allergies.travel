e619c6de1bc5556012b0932f495a99e8082b5d63
Neef Rehman <neef.rehman@gmail.com>
2018-10-07 15:28:10 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-// Collect all the portfolios into an array
+// Collect all the countries into an array
 
 const countryArray = [
   {
@@ -38,20 +38,6 @@ function kebabCase(string) {
 
 
 
-function shufflePortfolioArray() {
-
-  const a = portfolioArray;
-
-  var j, x, i;
-  for (i = a.length; i; i--) {
-    j = Math.floor(Math.random() * i);
-    x = a[i - 1];
-    a[i - 1] = a[j];
-    a[j] = x;
-  }
-  return a;
-}
-
 function createListElementsFromArray(a) {
   return a.map(function (el) {
     return "<li class='portfolio_item' data-filter='" + el.tags + "'><a target='_blank' href='#modal-" + kebabCase(el.name) + "'  rel='modal:open'>" + el.name + "</a></li>"
@@ -59,7 +45,6 @@ function createListElementsFromArray(a) {
 }
 
 function createModals() {
-  var list = shufflePortfolioArray();
 
   return list.map(function(el) {
     return "<div id='modal-" + kebabCase(el.name) + "'><div class='modal-inner'><div class='modal-details'><h2>" + el.name + "</h2><a class='modal-site-link' href='" + el.url + "' target='_blank'>" + el.tidyurl + "</a><p class='line'>" + el.line + "</p><p class='copy'>" + el.copy + "</p></div><div class='modal-image'><img class='modal-image-background' src='' data-src='" + el.image + "' /><img class='modal-image-logo' src='" + el.logo + "' /></div></div></div>"
@@ -112,12 +97,6 @@ $(document).ready(function () {
 
 
 
-$(".button").click(function(){
-  $(".button").not(this).removeClass('is_checked');
-  $(this).addClass("is_checked");
-});
-
-
 $(document).ready(function () {
   if (window.location.hash){
     var hash = window.location.hash.substring(1);
@@ -125,30 +104,29 @@ $(document).ready(function () {
     var array = $(".button-group").find(`[data-filter='${hash.toLowerCase()}']`);
     if (!array || !array.length) return;
     var el = array[0];
-    if (!el) return;
-    el.click();
+    var countryName, val;
+    for (var i=0; i<parameters.length;i++) {
+      val = parameters[i].split('=');
+      if (val[0] == 'team') {
+        countryName = val[1];
+      }
+    }
+    if (!countryName) return;
+    $('#' + countryName ).modal()
  }
 
   if (window.location.search){
     var query = window.location.search.substring(1);
     var parameters = query.split('&');
-    var companyName, val;
-    for (var i=0; i<parameters.length;i++) {
-      val = parameters[i].split('=');
-      if (val[0] == 'team') {
-        companyName = val[1];
-      }
-    }
-    if (!companyName) return;
-    $('#modal-' + companyName ).modal()
+    // var countryName, val;
+    // for (var i=0; i<parameters.length;i++) {
+    //   val = parameters[i].split('=');
+    //   if (val[0] == 'team') {
+    //     countryName = val[1];
+    //   }
+    // }
+    // if (!countryName) return;
+    // $('#' + countryName ).modal()
   }
 
-});
-
-
-$(document).ready(function(){
-  // if (window.location.search) return;
-  if (window.location.pathname == "/") {
-    history.replaceState( {} , 'allergies.travel', '/' );
-  }
 });
